df55e9e29789cc5f56209797b72cec3ddfa170de
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 12:30:59 +0200

    Applied fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- src/Builder/MySqlBuilder.php
+++ src/Builder/MySqlBuilder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Eloquent Schema.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Eloquent\Schema\Builder;
 
 use Illuminate\Database\Schema\MySqlBuilder as BaseBuilder;
--- src/Traits/BuilderTrait.php
+++ src/Traits/BuilderTrait.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Eloquent Schema.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Eloquent\Schema\Traits;
 
 use Closure;
@@ -59,7 +68,7 @@ trait BuilderTrait
         ($type === 'increments') ? $blueprint->{$type}($key) : $blueprint->{$type}($key)->primary();
 
         foreach ($this->config as $key => $value) {
-            if (!empty($value)) {
+            if (! empty($value)) {
                 if ($key === 'hashid') {
                     $blueprint->hashid('hashid')->index();
 
